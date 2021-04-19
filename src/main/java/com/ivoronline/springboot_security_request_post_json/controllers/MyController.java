5ab633977737dbb6be91ab750e50e2d83caabcd7
igoreta <ivoronline@gmail.com>
2021-04-19 09:05:55 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_security_request_post_json/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_request_post_json/controllers/MyController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MyController {
 
   @Autowired AuthenticationManager authenticationManager;
-  @Autowired private UserDetailsService userDetailsService;
 
   //========================================================================
   // AUTHENTICATE
